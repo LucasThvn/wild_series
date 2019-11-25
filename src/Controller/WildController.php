570b1c59ec3783daf727adb28c7917b8df0f68cc
LucasThvn <thauvinlucas@gmail.com>
2019-11-25 10:04:01 +0100

    création du route avec un nom de série.

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -13,10 +13,28 @@ class WildController extends AbstractController
      * @Route("/wild", name="wild_index")
      */
 
-    public function index() :Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+
+    /**
+     * @Route("/wild/show/{slug}",name="wild_show")
+     */
+
+    public function show(string $slug = ''): Response
+    {
+        if ($slug === '') {
+            $response = 'Aucune série sélectionnée, veuillez choisir une série';
+        } elseif (preg_match("/[A-Z]/", $slug)!=0 || strpos($slug, '_')) {
+        header('HTTP/1.0 404 Not Found');
+            exit;
+        } else {
+            $response = ucwords(implode(' ',explode('-', $slug)));
+        }
+        return $this->render('wild/show.html.twig', ['slug' => $response]);
     }
 }
